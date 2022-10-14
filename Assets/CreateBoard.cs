a92dec72620fdc5e491df1e554d824b6eaf51c28
Buck3tt <beckwalters@icloud.com>
2022-10-13 17:29:25 -0700

    first commit

---
+++ Assets/CreateBoard.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateBoard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private int xDimention;
+    [SerializeField]
+    private int yDimention;
+    [SerializeField]
+    private List<GameObject> gems;
+    [SerializeField]
+    private GameObject backGround;
+
+    [SerializeField]
+    private Transform cam;
+
+    private GameObject[,] board;
+
+    private Vector2Int[] swap;
+
+    void Start()
+    {
+        board = new GameObject[xDimention,yDimention];
+        generateGrid();
+        swap = new Vector2Int[2] {new Vector2Int(-1,-1), new Vector2Int(-1, -1)};
+    }
+
+    private void generateGrid()
+    {
+        for (int row = 0; row < xDimention; row++)
+        {
+            for (int col = 0; col < yDimention; col++)
+            {
+                int gem = Random.Range(0, gems.Count);
+                board[row, col] = Instantiate(gems[gem], new Vector3(row, col), Quaternion.identity);
+                board[row, col].transform.SetParent(transform);
+                board[row, col].name = $"{board[row, col].GetComponent<Gem>().GetGemType()} {row} {col}";
+                board[row, col].GetComponent<Gem>().pos = new Vector2Int(row, col);
+            }
+        }
+        Vector2 tileDimentions = board[0, 0].GetComponent<Gem>().GetTileSize();
+        cam.transform.position = new Vector3((float) xDimention/2 - tileDimentions.x/2f, (float) yDimention/2 - tileDimentions.y/2f, -10f);
+        var bg = Instantiate(backGround, new Vector3((float)xDimention / 2 - tileDimentions.x/2f, (float)yDimention / 2 - tileDimentions.y / 2f), Quaternion.identity);
+        bg.transform.localScale = new Vector3(xDimention + 2f, yDimention + 2f);
+    }
+
+    public Gem GetTile (Vector2Int pos)
+    {
+        return board[pos.x, pos.y].GetComponent<Gem>();
+    }
+
+    public void TilesToSwap(Gem gem)
+    {
+
+        if (swap[0] == new Vector2(-1f, -1f))
+        {
+            //Debug.Log("Player Selected "+gameObject.name);
+            swap[0] = gem.pos;
+        }
+        else
+        {
+            //Debug.Log("Player Selected " + gameObject.name);
+            if (!gem.isNeighbor(swap[0]))
+            {
+                Debug.Log("Tiles not next to eachother");
+                board[swap[0].x, swap[0].y].GetComponent<Gem>().setSelected(false);
+                gem.setSelected(false);
+            }
+            else {
+                swap[1] = gem.pos;
+
+                Swap();
+
+                
+            }
+            swap = new Vector2Int[2] { new Vector2Int(-1, -1), new Vector2Int(-1, -1) };
+        }
+    }
+
+    private void Swap()
+    {
+        GameObject temp1 = board[swap[0].x, swap[0].y];
+        GameObject temp2 = board[swap[1].x, swap[1].y];
+        Vector3 pos1 = temp1.transform.position;
+        Vector3 pos2 = temp2.transform.position;
+
+        board[swap[0].x, swap[0].y].transform.position = pos2;
+        board[swap[1].x, swap[1].y].transform.position = pos1;
+
+        board[swap[0].x, swap[0].y] = temp2;
+        board[swap[1].x, swap[1].y] = temp1;
+        temp1.GetComponent<Gem>().pos = swap[1];
+        temp2.GetComponent<Gem>().pos = swap[0];
+
+        temp1.GetComponent<Gem>().setSelected(false);
+        temp2.GetComponent<Gem>().setSelected(false);
+
+        temp1.GetComponent<Gem>().FindNeighbors();
+        temp2.GetComponent<Gem>().FindNeighbors();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Gem.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    [SerializeField]
+    private GemType type;
+
+    private GameObject outline;
+
+    [SerializeField]
+    private Vector2 dimentions = new Vector2(1f, 1f);
+
+    public Vector2Int pos;
+
+    private bool selected = false;
+
+    private List<Gem> adjacnceyList = new List<Gem>();
+
+    void Start()
+    {
+        outline = transform.Find("Selected").gameObject;
+        outline.SetActive(false);
+        FindNeighbors();
+    }
+
+    private void OnMouseEnter()
+    {
+        outline.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+        if (!selected)
+        {
+            outline.SetActive(false);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (!selected)
+        {
+            var c = gameObject.GetComponentInParent<CreateBoard>();
+            setSelected(true);
+            c.TilesToSwap(this);
+        }
+    }
+
+    public void setSelected (bool isSelected)
+    {
+        selected=isSelected;
+        outline.SetActive(isSelected);
+    }
+    public GemType GetGemType()
+    {
+        return type;
+    }
+
+    public Vector2 GetTileSize()
+    {
+        return dimentions;
+    }
+
+    public void Reset()
+    {
+        adjacnceyList.Clear();
+        selected = false;
+    }
+
+    public void FindNeighbors()
+    {
+        Reset();
+        CheckTile(Vector2.up);
+        CheckTile(Vector2.down);
+        CheckTile(Vector2.left);
+        CheckTile(Vector2.right);
+    }
+
+    public void CheckTile(Vector2 direction)
+    {
+        Vector2 halfExt = new Vector2(0.25f, 0.25f);
+        Collider2D[] collider2Ds = Physics2D.OverlapBoxAll(transform.position + (Vector3) direction, halfExt, 0f);
+
+        foreach (Collider2D item in collider2Ds)
+        {
+            Gem gem = item.GetComponent<Gem>();
+            if(gem != null)
+            {
+                adjacnceyList.Add(gem);
+            }
+        }
+    }
+    
+    public bool isNeighbor(Vector2Int pos)
+    {
+        foreach (Gem gem in adjacnceyList)
+        {
+            if(gem.pos == pos)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+   /*public List<Gem> FindMatches (Vector2 dir)
+    {
+        List<Gem> matches = new List<Gem>();
+   }*/
+}
+
+public enum GemType
+{
+    ICS, Math, Humanities, BioSci, Stats, Engineering
+}
